e4e7ee815d671cfbed115e46313910522dc5c17e
Matz <matzlarsson94@gmail.com>
2017-06-09 07:47:57 +0200

    Added blinking for LEDs

--- led.py
+++ led.py
@@ -1,5 +1,7 @@
 from hardware import Hardware
 import RPi.GPIO as GPIO
+import time
+import threading
 
 class LED(Hardware):
     
@@ -8,6 +10,8 @@ class LED(Hardware):
         self.pin = pin
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin, GPIO.OUT)
+        self.blinking = False
+        self.setValue(defaultValue)
         
     def getValue(self):
         return GPIO.input(self.pin)
@@ -23,3 +27,29 @@ class LED(Hardware):
         
     def setValue(self, value):
         GPIO.output(self.pin, value is not 0)
+        
+    # Note that the interval is for an entire cycle (on + off)    
+    def blink(self, interval=1.0, endState=0):
+        interval = float(interval)
+        if self.blinking:
+            self.blinkInterval = interval
+            self.blinkEndState = endState
+        else:
+            self.blinking = True
+            self.blinkInterval = interval
+            self.blinkEndState = endState
+            
+            def runBlink(self):
+                while(self.blinking):
+                    self.turnOn()
+                    time.sleep(self.blinkInterval/2)
+                    if self.blinking:                       # Make sure blink not cancelled
+                        self.turnOff()
+                        time.sleep(self.blinkInterval/2)
+                self.setValue(self.blinkEndState)
+            t = threading.Thread(target=runBlink, args=(self,))
+            t.daemon = True
+            t.start()
+            
+    def stopBlink(self):
+        self.blinking = False
