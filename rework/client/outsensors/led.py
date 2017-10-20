276c82037f3d5b1aa2f8e012a74cfb453502a10b
Matz <matzlarsson94@gmail.com>
2017-10-20 05:58:51 +0200

    Added LED functionality

--- rework/client/outsensors/led.py
+++ rework/client/outsensors/led.py
@@ -1,37 +1,63 @@
 from statusindicator import StatusIndicator
+import ioutil
+import threading
+import time
 
 class LED(StatusIndicator):
 
     def __init__(self, id, pin, blink, static):
         StatusIndicator.__init__(self, id)
-        self.blink_property = blink
-        self.static_property = static
+        self.bind_values = {
+            (blink, self.on_blink), (static, self.on_static)
+        }
+        self.pin = pin
+        self.blinking = False
+        self.io = ioutil
+        self.io.set_direction(self.pin, self.io.OUT)
         
     def set_publisher(self, event_engine):
         StatusIndicator.set_publisher(self, event_engine)
-        if not self.blink_property is None:
-            self.publisher.add_property_listener(self.blink_property, self.on_blink)
-        if not self.static_property is None:
-            self.publisher.add_property_listener(self.static_property, self.on_static)
+        for (prop, func) in self.bind_values:
+            if prop is not None:
+                self.publisher.add_property_listener(prop, func)
     
     
     def on_blink(self, prop, value):
         if value:
-            self.blink()
+            self.start_blink()
         else:
             self.off()
             
     def on_static(self, prop, value):
+        print("LED static called with val ", value)
         if value:
             self.on()
         else:
             self.off()
             
+    def start_blink(self):
+        if not self.blinking:
+            self.blinking = True
+            t = threading.Thread(target=self.blink, name="LED Blink")
+            t.daemon = True
+            t.start()
+        
     def blink(self):
-        print("BLINK BLINK")
+        sleep_time = 0.3
+        while(self.blinking):
+            self.set_value(1)
+            time.sleep(sleep_time)
+            if self.blinking:
+                self.set_value(0)
+                time.sleep(sleep_time)
         
     def on(self):
-        print("LED IS ON")
+        self.blinking = False
+        self.set_value(1)
         
     def off(self):
-        print("LED IS OFF")
+        self.blinking = False
+        self.set_value(0)
+        
+    def set_value(self, val):
+        self.io.write_pin(self.pin, val)
