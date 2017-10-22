0072f67d4af1827c1777ee3471c3fbbedb06b415
Matz <matzlarsson94@gmail.com>
2017-10-22 16:58:20 +0200

    Improved ultrasonic sensor stability

--- rework/client/sensors/ultrasonic.py
+++ rework/client/sensors/ultrasonic.py
@@ -4,9 +4,11 @@ import ioutil
 import threading
 from sensor import Sensor
 
+DIFF_LIMIT = 2
+
 class UltraSonic(Sensor):
 
-    def __init__(self, id, trigger, echo,  mindetect=0, maxdetect=1000, minsleep=-1, maxsleep=-1, allowedmisreads=3):
+    def __init__(self, id, trigger, echo,  sleep_time=2, read_freq_time=0.3, mindetect=0, maxdetect=1000, minsleep=-1, maxsleep=-1, allowedmisreads=3):
         Sensor.__init__(self, id)
         self.logger = logging.getLogger(__name__)
         self.trigger = trigger 
@@ -15,6 +17,9 @@ class UltraSonic(Sensor):
         self.value = 0
         self.io = ioutil
         
+        # Times between reads
+        self.sleep_time = sleep_time
+        self.read_freq_time = read_freq_time
         # Detection range that is valid
         self.min_distance = mindetect
         self.max_distance = maxdetect
@@ -48,11 +53,22 @@ class UltraSonic(Sensor):
         time.sleep(0.00001)
         self.io.write_pin(self.trigger, self.io.OFF)
 
-        while self.io.read_pin(self.echo)==0:
+        start_time = time.time()
+        failed_measurement = False
+        while self.io.read_pin(self.echo)==0 and not failed_measurement:
             pulse_start = time.time()
+            failed_measurement = (pulse_start - start_time) > 0.025
+            
+        if failed_measurement:
+            return 0
           
-        while self.io.read_pin(self.echo)==1:
+        start_time = time.time()
+        while self.io.read_pin(self.echo)==1 and not failed_measurement:
             pulse_end = time.time()
+            failed_measurement = (pulse_end - start_time) > 0.025
+            
+        if failed_measurement:
+            return 0
         
         pulse_duration = pulse_end - pulse_start
         distance = pulse_duration * 17150
@@ -67,26 +83,33 @@ class UltraSonic(Sensor):
         # Setup variables
         interval_size = self.max_distance-self.min_distance
         sleep_counter = 0
+        prev = 0
         
         # Loop for values until explicitly stopped
         while(self.continous_measure):
+            self.logger.debug("In ultrasonic loop")
             measure = self.do_measure()
             if measure > self.max_sleep_distance or measure < self.min_sleep_distance:
                 sleep_counter += 1
                 if sleep_counter > self.allowed_misreads:
                     self.logger.debug('ContinousMeasure: Measurement not in boundaries ({}cm). Waiting 2s'.format(measure))
-                    time.sleep(2)
+                    time.sleep(self.sleep_time)
                 else:
                     self.logger.debug('Increasing sleep counter to {}'.format(sleep_counter))
-                    time.sleep(0.01)
+                    time.sleep(self.read_freq_time)
             else:
                 if self.continous_measure:
                     sleep_counter = 0
-                    ratio = (measure-self.min_distance)/interval_size
-                    ratio = min(1, max(0, ratio))                     # Make sure it is between 0 and 1
-                    self.value = ratio
-                    self.push_event("change")
-                    time.sleep(0.01)
+                    if abs(prev-measure) < DIFF_LIMIT:
+                        ratio = (measure-self.min_distance)/interval_size
+                        ratio = min(1, max(0, ratio))                     # Make sure it is between 0 and 1
+                        self.value = ratio
+                        self.push_event("change")
+                    else:
+                        self.logger.debug("Ignores value due to error marginal")
+                    time.sleep(self.read_freq_time)
+            prev = measure
+        self.logger.debug("Stopping ultrasonic loop")
         
     def activate(self):
         self.continous_measure = True
